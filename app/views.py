45adad82c766ee71dab36190ab81e46169878ada
Matthew Hays <matthewhays419@gmail.com>
2024-10-23 22:10:23 -0500

    fix broken home link

--- app/views.py
+++ app/views.py
@@ -4,24 +4,30 @@ from django.http import HttpRequest
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "index.html")
+
 
 def beliefs(request):
-    return render(request, 'beliefs.html')
+    return render(request, "beliefs.html")
+
 
 def facebook(request):
-    return render(request, 'facebook.html')
+    return render(request, "facebook.html")
+
 
 def im_new(request):
-    return render(request, 'im_new.html')
+    return render(request, "im_new.html")
+
 
 def ministries(request):
-    return render(request, 'ministries.html')
+    return render(request, "ministries.html")
+
 
 def offering(request):
-    return render(request, 'offering.html')
+    return render(request, "offering.html")
+
 
 def staff(request):
-    return render(request, 'staff.html')
-
+    return render(request, "staff.html")
--- config/urls.py
+++ config/urls.py
@@ -14,17 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('beliefs/', views.beliefs, name='beliefs'),
-    path('facebook/', views.facebook, name='facebook'),
-    path('im_new/', views.im_new, name='im_new'),
-    path('ministries/', views.ministries, name='ministries'),
-    path('offering/', views.offering, name='offering'),
-    path('staff/', views.staff, name='staff'),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("beliefs/", views.beliefs, name="beliefs"),
+    path("facebook/", views.facebook, name="facebook"),
+    path("im_new/", views.im_new, name="im_new"),
+    path("ministries/", views.ministries, name="ministries"),
+    path("offering/", views.offering, name="offering"),
+    path("staff/", views.staff, name="staff"),
 ]
