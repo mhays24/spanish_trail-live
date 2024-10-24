e0444501d55e046444f4738a315cedab371b5078
Matthew Hays <matthewhays419@gmail.com>
2024-10-23 21:53:48 -0500

    first commit

---
+++ app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpRequest
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def beliefs(request):
+    return render(request, 'beliefs.html')
+
+def facebook(request):
+    return render(request, 'facebook.html')
+
+def im_new(request):
+    return render(request, 'im_new.html')
+
+def ministries(request):
+    return render(request, 'ministries.html')
+
+def offering(request):
+    return render(request, 'offering.html')
+
+def staff(request):
+    return render(request, 'staff.html')
+
