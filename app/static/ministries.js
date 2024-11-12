6c37f481dc78104656958e8a63d24197acd06866
Matthew Hays <matthewhays419@gmail.com>
2024-11-11 22:15:30 -0600

    small fixes

--- app/static/ministries.js
+++ app/static/ministries.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const sections = document.querySelectorAll('.slide-left, .slide-right');
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll('.slide-left, .slide-right');
 
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-            }
-        });
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
     });
+  });
 
-    sections.forEach(section => {
-        observer.observe(section);
-    });
+  sections.forEach(section => {
+    observer.observe(section);
+  });
 });
--- config/settings.py
+++ config/settings.py
@@ -125,9 +125,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'matthewhays419@gmail.com'  # Your email address
+EMAIL_HOST_USER = 'matthewhays419@gmail.com'  # the email used to send the messages
 EMAIL_HOST_PASSWORD = 'your-app-specific-password'  # App-specific password
-DEFAULT_FROM_EMAIL = 'matthewhays419@gmail.com'
+DEFAULT_FROM_EMAIL = 'matthewhays419@gmail.com' # the email messages sent to
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
