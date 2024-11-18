9feec21e50e0b23122506739aa38dc58242b8d4d
Matthew Hays <matthewhays419@gmail.com>
2024-11-18 11:54:20 -0600

    switch back to debug mode

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a-lswbxiw9@w1t^__k0_ori5^!pgq09*$dyblgof7_g+044r4q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
